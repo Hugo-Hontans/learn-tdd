0c567b7fecfb0002e462f369635e2eba17bfb161
Hugo Hontans <hugo.hontans@hiit-consulting.fr>
2023-02-24 11:42:13 +0100

    create spring boot app and angular app

---
+++ back-tdd/src/main/java/com/hugo/backtdd/BackTddApplication.java
@@ -0,0 +1,13 @@
+package com.hugo.backtdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackTddApplication.class, args);
+	}
+
+}
---
+++ back-tdd/src/test/java/com/hugo/backtdd/BackTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.backtdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
