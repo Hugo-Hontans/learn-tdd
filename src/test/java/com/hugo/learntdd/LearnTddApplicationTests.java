db54828cf953d2000a3069d618ef05392b056ea0
Hugo Hontans <hugo.hontans@hiit-consulting.fr>
2023-05-12 14:43:42 +0200

    initialize project

---
+++ src/test/java/com/hugo/learntdd/LearnTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.learntdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
